cf2bdbb09dfdfdc98a8f8432aae87917a9dd17b6
大木弘樹 <tamagodaijin@gmail.com>
2017-05-09 03:06:26 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php sesson_start(); ?>
+
+<!DOCTYPE <!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>テスト</title>
+</head>
+<body>
+  接続成功
+</body>
+</html>
